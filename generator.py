c0464c91dfc894ff5f976eaf5a8c8e72a760645c
ZalioSW <ztoxiumisntoxic@gmail.com>
2026-06-01 23:48:00 +0200

    Created the bits of the word

--- generator.py
+++ generator.py
@@ -4,17 +4,23 @@ from tkinter import messagebox
 root = tk.Tk()
 root.title("QR Code Generator")
 
+
+
+def convert_to_binary(number):
+    res = ""
+    while number > 0:
+        res += str(number%2)
+        number //= 2
+    return(res[::-1]).zfill(8)
+
 def generate():
-    bits = []
+    bits = "0100"
     text = entry.get()
-    for j, i in enumerate(text):
-        sideDict = dict(index = text[j], value = ord(i))
-        mainDict = dict(sideDict)
-        bits.append(mainDict)
+    bits += convert_to_binary(len(text)).zfill(8)
+    for i in text:
+        bits +=  convert_to_binary(ord(i))
+    bits += "0000"
     print(bits)
-
-
-
 
 
 frame = tk.Frame(root)
