c198d751285d021361da4285cb78b976ed5df202
ZalioSW <ztoxiumisntoxic@gmail.com>
2026-06-01 18:52:29 +0200

    Created a simple GUI and started on the binary conversion

--- generator.py
+++ generator.py
@@ -1,1 +1,29 @@
-print("Hello world")
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title("QR Code Generator")
+
+def generate():
+    bits = []
+    text = entry.get()
+    for j, i in enumerate(text):
+        sideDict = dict(index = text[j], value = ord(i))
+        mainDict = dict(sideDict)
+        bits.append(mainDict)
+    print(bits)
+
+
+
+
+
+frame = tk.Frame(root)
+frame.grid(row=0, column=0)
+
+entry = tk.Entry(frame)
+entry.grid(row=0, column=0)
+
+entry_btn = tk.Button(frame, text="Generate!", command=generate)
+entry_btn.grid(row=0, column=1)
+
+root.mainloop()
